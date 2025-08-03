777862a05d81414d6d00f3c1f88fdd37ad9a0448
Gigi <ganesanuflorin@yahoo.com>
2025-08-03 16:46:44 +0300

    Configured the swagger

--- src/main/java/com/product/manager/controller/ProductController.java
+++ src/main/java/com/product/manager/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.product.manager.controller;
 
 import com.product.manager.entity.Product;
 import com.product.manager.repository.ProductRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/products")
+@Tag(name = "Product Controller", description = "API for managing products")
 public class ProductController {
 
     private final ProductRepository productRepository;
 
     @PostMapping("/add")
+    @Operation(summary = "Add a new product", description = "Saves a new product to the database")
     public Product addProduct() {
         return productRepository.save(new Product());
     }
